a0def20a3135869a3b1f3cce1ec5d4e35970c3be
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-08-17 21:45:35 +0530

    testing: Error handling

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,10 @@ const port = process.env.PORT || 3000;
 app.get("/", (req: Request, res: Response) => {
 	res.send("Hello world!");
 });
-
+app.use((err: Error, req: Request, res: Response, next: any): any => {
+	console.error(err.stack);
+	res.status(500).send("Something broke!");
+});
 // start the Express server
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}`);
