220c790385618ec21080889450d62f9a0df22150
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-08-20 00:28:30 +0530

    test: CI/CD

--- server/app.ts
+++ server/app.ts
@@ -1,18 +1,45 @@
-import express, { Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-const app = express();
-const port = process.env.PORT || 3000;
-console.log(port);
-// define a route handler for the default home page
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello world!");
-});
-app.use((err: Error, req: Request, res: Response, next: any): any => {
-	res.status(500).send(err.stack);
-});
-// start the Express server
-app.listen(port, () => {
-	console.log(`server started at http://localhost:${port}`);
+var app = express();
+
+// view engine setup
+
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", function (req: Request, res: Response, next) {
+	return res.send("Hello World");
 });
 
-export default app;
+// catch 404 and forward to error handler
+app.use(function (req: Request, res: Response, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function (
+	err: any,
+	req: Request,
+	res: Response,
+	next: NextFunction
+): any {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
+});
+
+module.exports = app;
