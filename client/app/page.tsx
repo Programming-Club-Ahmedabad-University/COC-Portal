81d5103385c0957a5c82e020ab8b04cd2877df43
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-20 14:30:16 +0530

    chore: more smol modifications

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,9 +5,10 @@ import SignInButton from "./components/SignInButton";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className="text-9xl">45</div>
-      <Button color="primary">Click me</Button>
-      <SignInButton />
+      <div className="text-9xl">
+        45
+        <SignInButton />
+      </div>
     </main>
   );
 }
