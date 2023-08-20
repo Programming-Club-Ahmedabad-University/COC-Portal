9db1d3a2554f24c0d3b13eed2cdb19e6f4d0a3df
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-20 19:26:13 +0530

    feat: dummy user card to show data

---
+++ client/app/components/SignInButton/SignInButton.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Avatar, Button } from "@nextui-org/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React, { useState } from "react";
+import UserCard from "../UserCard/UserCard";
+
+function SignInButton() {
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState(false);
+
+  // if the user does exist:
+  if (session && session.user) {
+    return (
+      <>
+        <UserCard user={session.user} />
+
+        <Button color="primary" onClick={() => signOut()}>
+          Sign out
+        </Button>
+      </>
+    );
+  }
+
+  // if the user does not exist:
+  return (
+    <>
+      <h1>Sign in here</h1>
+
+      <Button
+        isLoading={loading}
+        color="primary"
+        onClick={async () => {
+          setLoading(true);
+          await signIn("google");
+        }}
+      >
+        {loading ? "Signin In..." : "Sign in with Google"}
+      </Button>
+    </>
+  );
+}
+
+export default SignInButton;
