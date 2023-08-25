497f29af22efa1341d4bc1f0bede9766506bf818
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-08-25 15:09:50 +0530

    feat: created new component

---
+++ client/pages/components/ScoreBoard/ScoreBoard.tsx
@@ -0,0 +1,11 @@
+import style from "./ScoreBoard.module.css";
+
+function ScoreBoard() {
+  return (
+    <div>
+      <h1 className={style.title}>ScoreBoard</h1>
+    </div>
+  );
+}
+
+export default ScoreBoard;
