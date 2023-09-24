0482d4cc748c3acf0a32938b0f63deeecb75a913
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-09-24 18:09:05 +0530

    api: add and list contests

---
+++ client/pages/Admin/LeaderboardConfig.tsx
@@ -0,0 +1,12 @@
+import RootLayout from "../Layout";
+import styles from "./LeaderboardConfig.module.css";
+
+export default function LeaderboardConfig() {
+	return (
+		<RootLayout>
+			<main className={styles.main}>
+				<h1>Contests on Leaderboard</h1>
+			</main>
+		</RootLayout>
+	);
+}
--- client/pages/components/NavBar/NavBar.tsx
+++ client/pages/components/NavBar/NavBar.tsx
@@ -1,23 +1,56 @@
 import {
-	Button,
-	Link,
+	Avatar,
 	Navbar,
 	NavbarBrand,
 	NavbarContent,
 	NavbarItem,
 } from "@nextui-org/react";
+import {
+	Dropdown,
+	DropdownTrigger,
+	DropdownMenu,
+	DropdownItem,
+	Button,
+} from "@nextui-org/react";
+import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import { useState } from "react";
 export default function NavBar() {
 	const { data: session } = useSession();
 	const [loading, setLoading] = useState(false);
+	const router = useRouter();
+
 	let authBtn;
+	const navItems = [
+		{
+			name: "Leaderboard",
+			link: "/",
+		},
+	];
 	if (session && session.user) {
+		if (session.user.role === "Admin") {
+			navItems.push({
+				name: "Leaderboard Config",
+				link: "/Admin/LeaderboardConfig",
+			});
+		}
 		authBtn = (
 			<>
-				<Button color="primary" onClick={() => signOut()}>
-					Sign out
-				</Button>
+				<Dropdown className="dark text-foreground ">
+					<DropdownTrigger>
+						<Avatar src={session.user.image!} />
+					</DropdownTrigger>
+					<DropdownMenu aria-label="Static Actions">
+						<DropdownItem
+							key="new"
+							color="danger"
+							onClick={() => signOut()}
+						>
+							Sign out
+						</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
 			</>
 		);
 	} else {
@@ -25,6 +58,7 @@ export default function NavBar() {
 			<Button
 				isLoading={loading}
 				color="primary"
+				variant="flat"
 				onClick={async () => {
 					setLoading(true);
 					await signIn("google");
@@ -41,26 +75,31 @@ export default function NavBar() {
 				<p className="font-bold text-inherit">COC </p>
 			</NavbarBrand>
 			<NavbarContent className="hidden sm:flex gap-4" justify="center">
-				<NavbarItem>
-					<Link color="foreground" href="#">
-						Leaderboards
-					</Link>
-				</NavbarItem>
-				<NavbarItem isActive>
-					<Link href="#" aria-current="page">
-						Customers
-					</Link>
-				</NavbarItem>
-				<NavbarItem>
-					<Link color="foreground" href="#">
-						Integrations
-					</Link>
-				</NavbarItem>
+				{navItems.map((item) => (
+					<NavbarItem key={item.name}>
+						<Link href={item.link}>
+							<div
+								className={
+									item.link != router.pathname
+										? "text-foreground"
+										: "text-primary"
+								}
+							>
+								{item.name}
+							</div>
+						</Link>
+					</NavbarItem>
+				))}
 			</NavbarContent>
 			<NavbarContent justify="end">
-				<NavbarItem className="hidden lg:flex">
-					<Link href="#">Login</Link>
-				</NavbarItem>
+				{session && session.user && (
+					<>
+						<div>
+							{session.user.name}
+							<small>{session.user.role}</small>
+						</div>
+					</>
+				)}
 				<NavbarItem>{authBtn}</NavbarItem>
 			</NavbarContent>
 		</Navbar>
