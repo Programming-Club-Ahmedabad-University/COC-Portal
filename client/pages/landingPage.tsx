b2123926ec80a7c299f06661e4fcf2ea2ac9e7b5
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-10-18 18:33:40 +0530

    some more progress on landing page

--- client/pages/landingPage.tsx
+++ client/pages/landingPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import RootLayout from "./Layout";
 import styles from "./styles/landingPage.module.css";
 import CodeDivider from "./components/CodeDivider/CodeDivider";
+import Image from "next/image";
 
 function landingPage() {
 	return (
@@ -12,6 +13,34 @@ function landingPage() {
 				</div>
 
 				<CodeDivider />
+
+				{/* card */}
+				<div className={styles.card}>
+					<Image src="/images/barb.png" alt="logo" width={350} height={350} />
+					<div className={styles.txt}>
+						<div className={styles.title}>
+							Lorem, ipsum dolor sit amet consectetur
+						</div>
+						<div>
+							<div className={styles.subTxt}>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci
+								nulla pellentesque dignissim enim sit amet venenatis urna
+								cursus. Morbi tristique senectus et netus et malesuada fames.
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<CodeDivider />
+
+				{/* card */}
+				<div></div>
+
+				<CodeDivider />
+
+				{/* card */}
+				<div></div>
 			</RootLayout>
 		</>
 	);
