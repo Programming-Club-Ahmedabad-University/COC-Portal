f41fe1c82e707b9c876d96c582378a779e9b95ae
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-10-18 18:48:08 +0530

    updated width

--- client/pages/components/CodeDivider/CodeDivider.tsx
+++ client/pages/components/CodeDivider/CodeDivider.tsx
@@ -5,10 +5,12 @@ import { AbsoluteCenter, Box, Divider } from "@chakra-ui/react";
 function CodeDivider() {
 	return (
 		<Box position="relative" padding="5" className={styles.divCont}>
-			<Divider borderWidth={"0.2em"} />
-			<AbsoluteCenter bg={"hsl(0, 0%, 10%)"} color={"white"} px="3">
-				<div className={styles.txt}>{"</>"}</div>
-			</AbsoluteCenter>
+			<div className={styles.longg}>
+				<Divider borderWidth={"0.2em"} />
+				<AbsoluteCenter bg={"hsl(0, 0%, 10%)"} color={"white"} px="3">
+					<div className={styles.txt}>{"</>"}</div>
+				</AbsoluteCenter>
+			</div>
 		</Box>
 	);
 }
