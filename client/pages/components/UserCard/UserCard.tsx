5202c8936d3301094eb8906d6d8d10af5b6c8ab2
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-10-19 22:40:19 +0530

    fix

--- client/pages/components/UserCard/UserCard.tsx
+++ client/pages/components/UserCard/UserCard.tsx
@@ -1,35 +1,35 @@
 "use client";
 
 import {
-  Avatar,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  Button,
+	Avatar,
+	Card,
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Button,
 } from "@chakra-ui/react";
 import { Session } from "next-auth";
 import React from "react";
 
 function UserCard({ user }: { user: Session["user"] }) {
-  return (
-    <Card>
-      <CardHeader>
-        <div>
-          <Avatar src={user?.image ?? ""} />
-          <div>
-            <h4>{user?.name}</h4>
-            <h5>{user?.email}</h5>
-            <h5>{user.role}</h5>
-          </div>
-        </div>
-      </CardHeader>
-      <CardBody>
-        <p>Haalo Clash of Codes Ramva!</p>
-      </CardBody>
-      <CardFooter></CardFooter>
-    </Card>
-  );
+	return (
+		<Card>
+			<CardHeader>
+				<div>
+					<Avatar src={user?.image ?? ""} />
+					<div>
+						<h4>{user?.name}</h4>
+						<h5>{user?.email}</h5>
+						<h5>{user?.role}</h5>
+					</div>
+				</div>
+			</CardHeader>
+			<CardBody>
+				<p>Haalo Clash of Codes Ramva!</p>
+			</CardBody>
+			<CardFooter></CardFooter>
+		</Card>
+	);
 }
 
 export default UserCard;
