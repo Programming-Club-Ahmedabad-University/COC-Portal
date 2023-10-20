2a159538a82c66db9fe4c8659a93637438ea3daa
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2023-10-20 20:46:32 +0530

    style: blurred

--- client/pages/components/BlurredCircle/BlurredCircle.tsx
+++ client/pages/components/BlurredCircle/BlurredCircle.tsx
@@ -1,15 +1,13 @@
 import styles from "./BlurredCircle.module.css";
 export default function BlurredCircle({
 	color,
-	height,
-	width,
+	
 	radius,
 	top,
 	left,
 }: {
 	color: string;
-	height: string;
-	width: string;
+	
 	radius: string;
 	top: string;
 	left: string;
@@ -19,8 +17,7 @@ export default function BlurredCircle({
 			className={styles.blurredCircle}
 			style={{
 				backgroundColor: color,
-				height: height,
-				width: width,
+				
 				borderRadius: radius,
 				left: left,
 				top: top,
