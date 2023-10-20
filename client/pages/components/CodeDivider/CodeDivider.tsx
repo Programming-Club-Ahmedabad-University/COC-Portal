6e31fb21f6a9114a4406c911e4fe55d57bad5fee
Sarthak Siddhpura <sarthaksiddhpura@gmail.com>
2023-10-20 13:17:27 +0530

    NEW CODE DIVIDER

--- client/pages/components/CodeDivider/CodeDivider.tsx
+++ client/pages/components/CodeDivider/CodeDivider.tsx
@@ -6,10 +6,9 @@ function CodeDivider() {
 	return (
 		<Box position="relative" padding="5" className={styles.divCont}>
 			<div className={styles.longg}>
-				<Divider borderWidth={"0.2em"} />
-				<AbsoluteCenter bg={"hsl(0, 0%, 10%)"} color={"white"} px="3">
-					<div className={styles.txt}>{"</>"}</div>
-				</AbsoluteCenter>
+				<div className={styles.divider}> </div>
+				<div className={styles.txt}>{"</>"}</div>
+				<div className={styles.divider}> </div>
 			</div>
 		</Box>
 	);
